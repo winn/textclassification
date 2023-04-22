184b1728b85f5f10262f2f167b756afd2899df04
vwinnv <vwinnv@gmail.com>
2023-04-22 17:30:49 +0700

    add

--- app.py
+++ app.py
@@ -7,9 +7,13 @@ import pickle
 from flask import Flask, request, jsonify
 from flask_restful import Resource, Api, reqparse
 from sklearn.feature_extraction.text import CountVectorizer
+import pandas as pd
 
 app = Flask(__name__)
 api = Api(app)
+
+url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRdDyIzjzb8uyiCEqkoBQlPychRQw64jUDruw64gbvlYzzoauGCljvhNNrRl-hqhx_uGER6FJDu0C4X/pub?gid=1104547283&single=true&output=csv'
+dat = pd.read_csv(url)
 
 @app.route('/')
 def index():
@@ -29,7 +33,37 @@ class get_classify(Resource):
         res = pipeline.predict([text])[0]
         return res
 
+def randrow(topic=None):
+    if topic!=None:
+        topic = topic.lower()
+        #return dat
+        sdat = dat[(dat.iloc[:,3].str.find('Health')!=-1) & (dat.iloc[:,3].notnull())]
+        res = sdat.sample(1).iloc[0].values
+    else:
+        res = dat.sample(1).iloc[0].values
+    topic = res[3]
+    prob = res[8]
+    sol = res[9]
+    out = {}
+    out['topic'] = topic
+    out['problem'] = prob
+    out['solution'] = sol
+    return out
 
+class get_data(Resource):
+    def get(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('topic', type=str)
+        dictp = parser.parse_args()
+        topic = dictp['topic']
+        out = randrow(topic)
+
+        return out
+
+
+
+
+api.add_resource(get_data, '/data',endpoint='data')
 api.add_resource(get_classify, '/classify',endpoint='classify')
 
 if __name__ == '__main__':
